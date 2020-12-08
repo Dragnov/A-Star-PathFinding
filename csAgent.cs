d591c7a785a15021c87de1f5fee1fbb37771797f
Dragnov <37103046+Dragnov@users.noreply.github.com>
2020-12-08 18:24:11 +0900

    Add files via upload

---
+++ csAgent.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MEC;
+
+/// <summary>
+/// 경로를 받아 실제로 이동을 수행하는 클래스
+/// </summary>
+public class csAgent : MonoBehaviour
+{
+	// 경로를 따라 이동하는 코루틴
+	private IEnumerator<float> m_CoFollowPath = null;
+
+	// 이동 완료 콜백
+	private Action m_Callback_FollowComplete = null;
+
+	private Transform m_Transform = null;
+
+	private CharacterController m_CharacterController = null;
+
+	// 타일맵을 생성하는 클래스로 현재 자신의 위치에 해당하는 타일을 받아오기 위한 용도
+	protected csGridCreator m_GridCreator = null;
+
+	// 경로찾기 기능
+	private csPathFinding m_PathFinding = null;
+
+	// 경로를 따라 회전
+	protected csRotationToTarget m_RotationToTarget = null;
+
+	public Vector3[] m_Path = null;
+	public Vector3 m_Target = Vector3.zero;
+	public Vector3 m_CurrentWayPoint = Vector3.zero;
+	public Vector3 m_LastWayPoint = Vector3.zero;
+
+	public float m_fFollowingSpeed = 4.0f;
+	public float m_fDelayCheckCycle = 0.0f;
+
+	public int m_iWayPointIndex;
+
+	public bool m_bIsFollowing = false;
+
+	protected virtual void Awake()
+	{
+		m_Transform = GetComponent<Transform>();
+		m_CharacterController.GetComponent<CharacterController>();
+		m_GridCreator = csPathRequestManager.Instance.GetComponent<csGridCreator>();
+		m_PathFinding = GetComponent<csPathFinding>();
+		m_RotationToTarget = GetComponent<csRotationToTarget>();
+	}
+
+	public void StartFollowPath(Vector3 _target, Action _callback_Complete = null)
+	{
+		m_Target = _target;
+
+		// 길찾기 요청
+		csPathRequestManager.RequestPath(m_PathFinding, m_Transform.position, m_Target, null, OnPathFound);
+
+		if (_callback_Complete != null)
+		{
+			m_Callback_FollowComplete = _callback_Complete;
+		}		
+	}
+
+	/// <summary>
+	/// 경로 이동이 완료됐을 때, 혹은 AI 상태에 따라서 이동을 멈춰야 할 때 호출
+	/// </summary>
+	public void StopFollowPath()
+	{
+		if (m_CoFollowPath != null)
+		{
+			StopCoroutine(m_CoFollowPath);
+			m_CoFollowPath = null;
+		}
+
+		if (m_Callback_FollowComplete != null)
+		{
+			m_Callback_FollowComplete = null;
+		}
+
+		m_PathFinding.Initialize();
+
+		m_Path = default;
+
+		m_bIsFollowing = false;
+	}
+
+	/// <summary>
+	/// 경로 찾기 결과 콜백 함수
+	/// </summary>
+	/// <param name="_newPath"></param>
+	/// <param name="_success"></param>
+	private void OnPathFound(Vector3[] _newPath, bool _success)
+	{
+		if (_success)
+		{
+			m_bIsFollowing = true;
+
+			m_iWayPointIndex = 0;
+			m_Path = _newPath;
+
+			m_LastWayPoint = m_Path[m_Path.Length - 1];
+
+			ChangeCurrentWayPoint(0);
+
+			// 현재 이동 중이 아닐 때만 코루틴 호출
+			if (m_CoFollowPath == null)
+			{
+				m_CoFollowPath = CoFollowPath();
+				StartCoroutine(m_CoFollowPath);
+			}
+
+			return;
+		}
+	}
+
+	private void ChangeCurrentWayPoint(int _index)
+	{
+		m_CurrentWayPoint = m_Path[_index];
+
+		Vector3 RelativePositionFromMeToWayPoint = Utility.RelativePosition(new Vector3(m_Transform.position.x, 0.0f, m_Transform.position.z), m_CurrentWayPoint);
+
+		// 이동할 웨이포인트 쪽을 바라봄
+		m_RotationToTarget.Rotation(m_Transform, RelativePositionFromMeToWayPoint);
+	}
+
+	#region Coroutine
+
+	private IEnumerator<float> CoFollowPath()
+	{
+		Vector3 PrevPosition = Vector3.zero;
+
+		float Timer = 0.0f;
+		float DistanceFromMeToWayPoint = 0.0f;
+
+		while (true)
+		{
+			yield return Timing.WaitForOneFrame;
+
+			Timer += Time.deltaTime;
+
+			// 일정 시간마다 이동한 거리를 체크하여 원래 이동했어야 할 거리보다 작다면
+			// 예기치 못한 충돌이 발생했다는 의미이므로 경로를 다시 요청
+			if (Timer >= m_fDelayCheckCycle)
+			{
+				float DistanceFromPrev = Utility.Distance(PrevPosition, m_Transform.position);
+				PrevPosition = m_Transform.position;
+
+				if (DistanceFromPrev < (m_fFollowingSpeed * m_fDelayCheckCycle) - 0.2f)
+				{
+					csPathRequestManager.RequestPath(m_PathFinding, m_Transform.position, m_Target, m_GridCreator.NodeFromWorldPoint_Unit(m_CurrentWayPoint), OnPathFound);
+				}
+
+				Timer = 0.0f;
+			}
+
+			DistanceFromMeToWayPoint = Utility.Distance(m_Transform.position, m_CurrentWayPoint);
+
+			// 목적지 도착
+			if (m_iWayPointIndex == m_Path.Length)
+			{
+				m_Callback_FollowComplete?.Invoke();
+
+				StopFollowPath();
+
+				break;
+			}
+			else
+			{
+				// 현재 웨이포인트에 도달했다면 다음 포인트로 바꿔줌
+				if (DistanceFromMeToWayPoint == 0.0f)
+				{
+					m_iWayPointIndex++;
+
+					if (m_iWayPointIndex < m_Path.Length)
+					{
+						ChangeCurrentWayPoint(m_iWayPointIndex);
+					}
+				}
+
+				Vector3 RelativePosition = Utility.RelativePosition(m_Transform.position, m_CurrentWayPoint);
+				Vector3 Direction = RelativePosition.normalized * m_fFollowingSpeed * Time.deltaTime;
+
+				m_CharacterController.Move(Direction);
+			}
+		}
+	}
+
+	#endregion
+}
---
+++ csPathFinding.cs
@@ -0,0 +1,260 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+/// <summary>
+/// 경로를 찾아서 전달해주는 클래스
+/// </summary>
+public class csPathFinding : MonoBehaviour
+{
+	// 경로 탐색을 수행하는 코루틴
+	private IEnumerator m_CoFindPath = null;
+
+	// 예기치 못한 이동 방해 요소를 제외시키기 위함
+	private List<csNode> m_ListExceptionNode = new List<csNode>();
+
+	// 타일맵을 생성하는 클래스
+	private csGridCreator m_GridCreator = null;
+
+	private csNode m_StartNode = null;
+	public csNode m_EndNode = null;
+
+	protected virtual void Start()
+	{
+		m_GridCreator = csPathRequestManager.Instance.GetComponent<csGridCreator>();
+	}
+
+	public void Initialize()
+	{
+		for (int i = 0; i < m_ListExceptionNode.Count; i++)
+		{
+			m_ListExceptionNode[i].ResetType(gameObject.GetInstanceID());
+		}
+
+		m_ListExceptionNode.Clear();
+	}
+
+	public void StartFindPath(Vector3 _startPosition, Vector3 _endPosition, csNode _exceptionNode)
+	{
+		if (m_CoFindPath != null)
+		{
+			StopCoroutine(m_CoFindPath);
+		}
+
+		m_CoFindPath = CoFindPath(_startPosition, _endPosition, _exceptionNode);
+		StartCoroutine(m_CoFindPath);
+	}
+
+	private IEnumerator CoFindPath(Vector3 _startPosition, Vector3 _endPosition, csNode _exceptionNode)
+	{
+		// 요청을 받을 때 만약 탐색에 제외할 노드를 전달 받으면 저장했다가 탐색에 제외시킴
+		if (_exceptionNode != null)
+		{
+			if (!m_ListExceptionNode.Contains(_exceptionNode))
+			{
+				_exceptionNode.SetType(gameObject.GetInstanceID(), eNodeType.Obstacle);
+				m_ListExceptionNode.Add(_exceptionNode);
+			}
+		}
+
+		bool IsPathSuccess = false;
+
+		if (m_StartNode != null)
+		{
+			m_StartNode.ResetType(gameObject.GetInstanceID());
+		}
+
+		if (m_EndNode != null)
+		{
+			m_EndNode.ResetType(gameObject.GetInstanceID());
+		}
+
+		m_StartNode = m_GridCreator.NodeFromWorldPoint_Unit(_startPosition);
+		m_EndNode = m_GridCreator.NodeFromWorldPoint_Unit(_endPosition);
+
+		m_EndNode.SetType(gameObject.GetInstanceID(), eNodeType.End);
+
+		if (m_EndNode.NodeType != eNodeType.Obstacle)
+		{ 
+			Heap<csNode> OpenList = new Heap<csNode>(m_GridCreator.m_Grid_Unit.MaxSize);
+			HashSet<csNode> CloseSet = new HashSet<csNode>();
+
+			OpenList.Add(m_StartNode);
+
+			while (OpenList.Count > 0)
+			{
+				csNode Node = OpenList.RemoveFirst();
+
+				CloseSet.Add(Node);
+
+				// 경로 탐색 완료
+				if (Node == m_EndNode)
+				{
+					IsPathSuccess = true;
+
+					break;
+				}		
+
+				for (int i = 0; i < Node.m_ListNeighbours.Count; i++)
+				{
+					csNode Neighbour = Node.m_ListNeighbours[i];
+				
+					if (CloseSet.Contains(Neighbour))
+					{
+						continue;
+					}
+
+					if (CheckNeighbours(Node, Neighbour, i))
+					{
+						CloseSet.Add(Neighbour);
+
+						continue;
+					}
+
+					int NewCostNeighbour = Node.G + GetDistance(Node, Neighbour);
+
+					if (NewCostNeighbour < Neighbour.G || !OpenList.Contains(Neighbour))
+					{
+						Neighbour.G = NewCostNeighbour;
+						Neighbour.H = GetDistance(Neighbour, m_EndNode);
+						Neighbour.m_Parent = Node;
+
+						if (!OpenList.Contains(Neighbour))
+						{
+							OpenList.Add(Neighbour);
+						}
+					}
+				}
+			}
+		}
+
+		yield return null;
+
+		Vector3[] WayPoints;
+
+		if (IsPathSuccess)
+		{
+			List<csNode> ListPathNodes = RetracePath(m_StartNode, m_EndNode);
+
+			List<Vector3> ListWayPoint = new List<Vector3>();
+
+			// 경로에 포함된 노드들의 위치만 추출하여 저장
+			for (int i = 0; i < ListPathNodes.Count; i++)
+			{
+				ListWayPoint.Add(ListPathNodes[i].m_WorldPosition);
+			}
+
+			WayPoints = ListWayPoint.ToArray();
+
+			// 경로가 현재 역순이므로 
+			Array.Reverse(WayPoints);
+		}
+
+		// 경로 탐색 결과 전달
+		csPathRequestManager.Instance.FinishedProcessingPath(WayPoints, IsPathSuccess);
+
+		m_CoFindPath = null;
+	}
+
+	/// <summary>
+	/// 오픈된 노드의 주변 노드가 오픈할 수 있는지 확인
+	/// </summary>
+	/// <param name="_open"></param>
+	/// <param name="_neighbour"></param>
+	/// <param name="_index"></param>
+	/// <returns></returns>
+	private bool CheckNeighbours(csNode _open, csNode _neighbour, int _index)
+    {
+		// 제외 대상이라면
+		if (m_ListExceptionNode.Contains(_neighbour))
+		{
+			return false;
+		}
+
+		// 장애물이라면
+		if (_neighbour.NodeType == NodeType.Obstacle)
+		{
+			return false;
+		}
+
+		// 대각선일 때
+		if (_index % 2 != 0)
+		{
+			/* 1 0
+			 * 0 1
+			 * 왼쪽 상단의 1이 Open Node 오른쪽 하단의 1이 현재 확인하려는 인접노드, 즉 대각선일 때
+			 * 0에 해당하는 노드를 얻음
+			 */
+			csNode NodeA = m_GridCreator.m_Grid_Unit.Grid[_neighbour.m_Grid.x, _open.m_Grid.y];
+			csNode NodeB = m_GridCreator.m_Grid_Unit.Grid[_open.m_Grid.x, _neighbour.m_Grid.y];
+
+			/* 해당하는 노드들이 이동할 수 있는 없는 노드인지 확인
+			 * 이것은 노드상으로는 이동이 가능하다고 판단되지만 경로는 이동시키는 개체의 크기를 고려하지 않으므로
+			 * 추가로 확인하는 작업
+			 */
+			if (NodeA.NodeType != eNodeType.Walkable || NodeB.NodeType != eNodeType.Walkable)
+			{
+				Vector3 BetweenNodePosition = (_open.m_WorldPosition + _neighbour.m_WorldPosition) / 2.0f;
+
+				// 그럼에도 불구하고도 실제로 이동이 불가능한지 확인
+				if (IsRealImpossible(BetweenNodePosition))
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/// <summary>
+	/// 전달받은 위치가 실제로 이동이 불가능한지 확인
+	/// </summary>
+	/// <param name="_position"></param>
+	/// <returns></returns>
+	private bool IsRealImpossible(Vector3 _position)
+	{
+		if (Physics.CheckBox(_position, Vector3.one * m_GridCreator.m_Grid_Unit.NodeRadius, Quaternion.identity, csBattleManager.Instance.m_iLayerMask_Map))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	/// <summary>
+	/// 목적지부터 출발지까의 노드들을 추적하여 저장 후 반환
+	/// </summary>
+	/// <param name="_startNode"></param>
+	/// <param name="_endNode"></param>
+	/// <returns></returns>
+	private List<csNode> RetracePath(csNode _startNode, csNode _endNode)
+	{
+		List<csNode> ListPath = new List<csNode>();
+
+		csNode CurrentNode = _endNode;
+
+		while (CurrentNode != _startNode)
+		{
+			ListPath.Add(CurrentNode);
+			CurrentNode = CurrentNode.m_Parent;
+		}
+
+		return ListPath;
+	}
+
+	private int GetDistance(csNode _nodeA, csNode _nodeB)
+	{
+		int DistanceX = Mathf.Abs(_nodeA.m_Grid.x - _nodeB.m_Grid.x);
+		int DistanceY = Mathf.Abs(_nodeA.m_Grid.y - _nodeB.m_Grid.y);
+
+		if (DistanceX > DistanceY)
+		{
+			return 14 * DistanceY + 10 * (DistanceX - DistanceY);
+		}
+
+		return 14 * DistanceX + 10 * (DistanceY - DistanceX);
+	}
+}
